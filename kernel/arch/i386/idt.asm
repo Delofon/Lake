8a5b26160b84339976109d1e736c45202e8c20df
Delofon <spacedelofon@gmail.com>
2024-12-10 02:53:41 +0300

    idt begin

---
+++ kernel/arch/i386/idt.asm
@@ -0,0 +1,20 @@
+section .bss
+global idtp
+idtp:
+    resq 80
+idt_end:
+idt_size equ idt_end - idtp
+
+section .data
+idtr:
+    dw idt_size - 1
+    dd idtp
+
+section .text
+global setidt
+setidt:
+    cli
+    lidt [idtr]
+
+    ret
+
