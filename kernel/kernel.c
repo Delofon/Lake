36115620b687101ed1402556deb734328228043d
Delofon <spacedelofon@gmail.com>
2024-12-13 18:01:54 +0300

    Get rid of now redudnant debug prints

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -13,10 +13,6 @@ void kmain(uint8_t *gdtp, uint32_t *idtp)
 {
     printf("Lake v3\n\n");
 
-    printf("idtp+0x0:  0x%08x 0x%08x\n", idtp[0x0*2+1], idtp[0x0*2]);
-    printf("idtp+0x8:  0x%08x 0x%08x\n", idtp[0x8*2+1], idtp[0x8*2]);
-    printf("idtp+0x80: 0x%08x 0x%08x\n", idtp[0x80*2+1], idtp[0x80*2]);
-
     for(;;);
 }
 
